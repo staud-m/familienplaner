603b7b413fd147709bc5e5dfcf07dd60ab818a27
max <max@Air-von-max.fritz.box>
2026-05-20 17:56:59 -0700

    erstelle Tabelle Event

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
 
-from sqlalchemy import Boolean, Column, DateTime, String
+from sqlalchemy import Boolean, Column, DateTime, String, Integer
 from sqlalchemy.dialects.postgresql import UUID
 
 from app.database import Base
@@ -15,3 +15,12 @@ class ShoppingItem(Base):
     category = Column(String(100), nullable=False)
     is_done = Column(Boolean, default=False, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+
+
+class Event(Base):
+    __tablename__="event"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    category = Column(String(100), nullable=False)
+    amount=Column(Integer,nullable=False)
+    balance=Column(Integer,nullable=False)
+    date = Column(DateTime, default=datetime.utcnow, nullable=False)
